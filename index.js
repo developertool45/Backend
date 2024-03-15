4e25aa1c11eb6be38353b9dd0f029b28cccbb358
developertool45 <developertool45@gmail.com>
2024-03-15 17:30:46 +0530

    add files

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ app.listen(port, () => {
 
 app.get("/:username/:id", (req, res) => {
     let { username, id } = req.params;
-    console.log(req.params);
+    // console.log(req.params);
     let htmlStr = `<h1> Welcome of the page of @${username}${id}</h1> `;   
     res.send(htmlStr)
+})
+
+
+app.get("/search", (req, res) => {
+    // console.log(req.query);
+    let { q } = req.query;
+    res.send(`result of query : ${q}`)
 })
